707ba40ab8b19f5ac5c5deb3ea930d8af7a4c20a
José <josedemelo.if@gmail.com>
2018-10-31 15:30:35 -0300

    [UP] Updating basics settings

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+let i = 0;
+
+$('.carousel').carousel();
+
+function onClick(){
+    if(i == 0){
+        $("#main").hide();
+        i = 1;
+    }else{
+        $("#main").show();
+        i = 0;
+    }
+}
